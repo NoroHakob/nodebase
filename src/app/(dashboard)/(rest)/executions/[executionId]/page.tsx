d91251aa758e254d3d947e54861707f0fead86df
NoroHakob <norohakob2004@gmail.com>
2026-06-05 15:39:26 +0400

    Executions History

--- src/app/(dashboard)/(rest)/executions/[executionId]/page.tsx
+++ src/app/(dashboard)/(rest)/executions/[executionId]/page.tsx
@@ -1,20 +1,36 @@
-"use client"
-
-import { requireAuth } from "@/lib/auth-utils"
-import { useParams } from "next/navigation"
+import { ExecutionView } from "@/features/executions/components/execution";
+import { ExecutionsError, ExecutionsLoading } from "@/features/executions/components/executions";
+import { prefetchExecution } from "@/features/executions/server/prefetch";
+import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface PageProps {
-    params: {
-        executionId: string
-    }
-}
+  params: Promise<{
+    executionId: string;
+  }>
+};
 
-const Page = async () => {
-    await requireAuth()
+const Page = async ({ params }: PageProps) => {
+  await requireAuth();
+  
+  const { executionId } = await params;
+  prefetchExecution(executionId);
 
-    const params = useParams()
-    
-    return <p>Execution id: {params.executionId}</p>
-}
+  return (
+    <div className="p-4 md:px-10 md:py-6 h-full">
+      <div className="mx-auto max-w-screen-md w-full flex flex-col gap-y-8 h-full">
+        <HydrateClient>
+          <ErrorBoundary fallback={<ExecutionsError />}>
+            <Suspense fallback={<ExecutionsLoading />}>
+              <ExecutionView executionId={executionId} />
+            </Suspense>
+          </ErrorBoundary>
+        </HydrateClient>
+      </div>
+    </div>
+  )
+};
 
 export default Page;
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -2,21 +2,42 @@
   import { inngest } from "./client";
   import prisma from "@/lib/db";
   import { topologicalSort } from "./utils";
-  import { NodeType } from "@/generated/prisma";
+  import { ExecutionStatus, NodeType } from "@/generated/prisma";
   import { getExecutor } from "@/features/executions/lib/executor-registry";
 
   export const executeWorkflow = inngest.createFunction(
     { 
       id: "execute-workflow",
       triggers: [{ event: "workflows/execute.workflow" }],
-      retries: 0
+      retries: 0,
+      onFailure: async ({ event, step }) => {
+        return prisma.execution.update({
+          where: { inngestEventId: event.data.event.id },
+          data: {
+            status: ExecutionStatus.FAILED,
+            error: event.data.error.message,
+            errorStack: event.data.error.stack,
+          },
+        });
+      },
     },
+
     async ({ event, step }) => {
+      const inngestEventId = event.id;
       const workflowId = event.data.workflowId;
 
-      if (!workflowId) {
-        throw new NonRetriableError("Workflow ID is missing");
+      if (!inngestEventId || !workflowId) {
+        throw new NonRetriableError("Event ID or workflow ID is missing");
       }
+
+      await step.run("create-execution", async () => {
+        return prisma.execution.create({
+          data: {
+            workflowId,
+            inngestEventId,
+          },
+        });
+      });
 
       const sortedNodes  = await step.run("prepare-workflow", async () => {
         const workflow = await prisma.workflow.findUniqueOrThrow({
@@ -54,6 +75,17 @@
           publish: step.realtime.publish.bind(step.realtime),
         });
       }
+
+      await step.run("update-execution", async () => {
+        return prisma.execution.update({
+          where: { inngestEventId, workflowId },
+          data: {
+            status: ExecutionStatus.SUCCESS,
+            completedAt: new Date(),
+            output: context,
+          },
+        })
+      });
 
       return {
         workflowId,
